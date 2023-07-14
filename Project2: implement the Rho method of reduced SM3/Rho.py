7f8e6b42f5122624e649605787940793d75b413c
Innovation_and_Entrepreneurship_Practice_Group_100_tangshi <102496996+BTangEShi@users.noreply.github.com>
2023-07-14 13:12:00 +0800

    Create Rho.py

---
+++ Project2: implement the Rho method of reduced SM3/Rho.py
@@ -0,0 +1,142 @@
+import hashlib
+from gmssl import sm3
+from string import ascii_letters, digits
+from itertools import permutations
+from time import time
+ts='zxcvbnmasdfghjklpoiuytrewq0987654321'
+st={}
+def collision_SHA256():
+    for item in permutations(ts,5):
+        item=''.join(item)
+        item=item.encode()
+        hash_object = hashlib.sha256(item)
+        hex_digest = hash_object.hexdigest()
+        value=hex_digest[0:6]
+        if(value not in st):
+            st[value]=(item,1)
+        else:
+            print(st[value][0])
+            print(item)
+            print(value)
+            break
+def collision_SM3():
+    for item in permutations(ts,5):
+        item=''.join(item)
+        item=item.encode()
+        item_list=[i for i in item]
+        hex_digest = sm3.sm3_hash(item_list)
+        value=hex_digest[0:6]
+        if(value not in st):
+            st[value]=(item,1)
+        else:
+            print(st[value][0])
+            print(item)
+            print(value)
+            break
+def SM3(item):
+    #item=item.encode()
+    item_list=[i for i in item]
+    hex_digest = sm3.sm3_hash(item_list)
+    Hash=hex_digest.encode()
+    return Hash
+
+def SHA256(item):
+    #item=item.encode()
+    hash_object = hashlib.sha256(item)
+    hex_digest = hash_object.hexdigest()
+    return hex_digest.encode()
+
+def Rho_SM3(num):
+    item=b"202100460120"
+    Hash1=SM3(item)
+    Hash2=SM3(Hash1)
+    while(True):
+        if(Hash1[0:num]!=Hash2[0:num]):
+            Hash1=SM3(Hash1)
+            Hash2=SM3(SM3(Hash2))
+            
+        else:
+            print(Hash1)
+            print(Hash2)
+            break
+
+def Rho_SHA256(num):
+    item=b"202100460120"
+    Hash1=SHA256(item)
+    Hash2=SHA256(Hash1)
+    while(True):
+        if(Hash1[0:num]!=Hash2[0:num]):
+            Hash1=SHA256(Hash1)
+            Hash2=SHA256(SHA256(Hash2))
+        else:
+            print(Hash1)
+            print(Hash2)
+            break
+        
+def Random_SHA256(num):
+    item=b"202100460120"
+    jk=False
+    Hash_value=SHA256(item)
+    for item in permutations(ts,5):
+        item=''.join(item)
+        item=item.encode()
+        hash_object = hashlib.sha256(item)
+        hex_digest = hash_object.hexdigest().encode()
+        if(hex_digest[0:num]==Hash_value[0:num]):
+            jk=True
+            print(item)
+            print(hex_digest)
+        if(jk==True):
+            break
+
+
+
+start = time()
+Rho_SHA256(2)
+print('Time used:', time()-start)
+
+start = time()
+Rho_SHA256(4)
+print('Time used:', time()-start)
+
+start = time()
+Rho_SHA256(6)
+print('Time used:', time()-start)
+
+start = time()
+Rho_SHA256(8)
+print('Time used:', time()-start)
+
+
+start = time()
+collision_SM3()
+print('Time used:', time()-start)
+
+'''
+start = time()
+collision_SHA256(10)
+print('Time used:', time()-start)
+
+
+'''
+'''
+#input_s=input("请输入字符串：")
+start=time()
+for i in range(1000):
+    SM3("ssss")
+print('Time used:', time()-start)
+
+start=time()
+for i in range(1000):
+    SHA256("ssss")
+print('Time used:', time()-start)
+
+
+'''
+'''
+print(SM3("202100460120202100460120202100460120202100460120202100460120"))
+print(SM3("20210046012020210046012020210046012020210046012020210046012"))
+
+
+print(SM3("2021004601202021004601202021004601202021004601202021004601"))
+'''
