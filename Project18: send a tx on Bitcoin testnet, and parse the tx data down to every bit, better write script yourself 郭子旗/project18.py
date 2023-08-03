408e7228ec3b0ffe22268f8d1d5223a925cafff2
hellowold32234 <139443157+hellowold32234@users.noreply.github.com>
2023-08-04 01:53:21 +0800

    Delete Project18: send a tx on Bitcoin testnet, and parse the tx data down to every bit, better write script yourself 郭子旗 directory

--- Project18: send a tx on Bitcoin testnet, and parse the tx data down to every bit, better write script yourself 郭子旗/project18.py
+++
@@ -1,9 +0,0 @@
-from bit import PrivateKeyTestnet
-my_key = PrivateKeyTestnet('cSKxS5jmxCHK6Cry9xxnCEveuFed62svVV3hsYFHdiEeXEmiFG6d')
-print(my_key)
-print(my_key.version)
-print(my_key.to_wif())
-print(my_key.address)
-#print(my_key.balance_as('usd'))
-
-print(my_key.create_transaction([('mjJEoUh5T8GjqcGndrXWFAWQahpHoMq8s1', 190, 'usd')]))
